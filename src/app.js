ba3f906647045fbc4216a61cccd675ce496161d0
Cody Abbott <cody.abbott16@gmail.com>
2020-01-14 16:01:01 +0000

    able to select country and display info

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ document.addEventListener('DOMContentLoaded', () => {
     data: {
       countryURL: null,
       countries: [],
-      seen: false
+      flagURL: null,
+      seen: false,
+      selectedCountry: null
     },
     mounted() {
       this.fetchCountries();
@@ -16,7 +18,13 @@ document.addEventListener('DOMContentLoaded', () => {
         fetch('https://restcountries.eu/rest/v2/all')
           .then(response => response.json())
           .then(data => this.countries = data)
-      }
+      },
+      fetchFlag: function(){
+        fetch('https://restcountries.eu/rest/v2/all')
+          .then(response => response.json())
+          .then(data => this.flagURL = data.flag)
+      },
+
     }
   })
 });
